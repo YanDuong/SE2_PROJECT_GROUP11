a9436b500a01495cc947717553bd65ab84d55a10
YanDuong <58656986+YanDuong@users.noreply.github.com>
2020-04-06 20:28:17 +0700

    Delete CityCrud.java

--- code/src/dao/CityCrud.java
+++
@@ -1,9 +0,0 @@
-package dao;
-import java.util.List;
-import model.City;
-public interface CityCrud {
-void addCity(City c);
-boolean deleteCity(int cityId);
-boolean updateCity(City city);
-List<City> getAllCities();
-}
