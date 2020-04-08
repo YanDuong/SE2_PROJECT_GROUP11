1b03cf70dd6337fb47649aa7d8637b4254106d5d
YanDuong <58656986+YanDuong@users.noreply.github.com>
2020-04-08 21:23:54 +0700

    Add files via upload

---
+++ code/src/dao/CityCrud.java
@@ -0,0 +1,16 @@
+package dao;
+import java.util.List;
+import model.City;
+public interface CityCrud {
+void addCity(City c);
+boolean deleteCity(int cityId);
+boolean updateCity(City city);
+List<City> getAllCities();
+City getCity(int id);
+int getTotalOfInfected();
+int getTotalOfRecovered();
+int getTotalOfDeaths();
+int getUpdatedInfected();
+int getUpdatedRecovered();
+int getUpdatedDeaths();
+}
